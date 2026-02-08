9b5177007867c8a0070178379ac1afe7aa8cbcb5
Samuel <samuel29033008@gmail.comm>
2026-02-08 14:32:54 -0600

    Add dashboard stats model

---
+++ mobile/lib/models/dashboard_stats.dart
@@ -0,0 +1,11 @@
+class DashboardStats {
+  final double revenue;
+  final int impressions;
+  final double eCPM;
+
+  DashboardStats({
+    required this.revenue,
+    required this.impressions,
+    required this.eCPM,
+  });
+}
